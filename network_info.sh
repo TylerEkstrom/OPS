352f959a2b975eef91b106f47e344677da90c935
Tyler Ekstrom <tylerxtreme@gmail.com>
2021-11-30 15:29:08 -0700

    edit

--- network_info.sh
+++ network_info.sh
@@ -1,9 +1,7 @@
-# this files does things
+# This file creates a txt file with network information about the device
 # author: Tyler 
 # Last updated 11/30/2021    
 
 #!/usr/bin/env bash
-
-# creates a txt file with network information about the device
  
 echo "`ifconfig`" > output.txt
