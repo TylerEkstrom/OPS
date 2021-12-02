be2c1c9b8769cbc65d26d83c67b0094d0c9bea63
Tyler Ekstrom <tylerxtreme@gmail.com>
2021-12-02 16:34:04 -0700

    edited

--- create_directory.sh
+++ create_directory.sh
@@ -3,6 +3,6 @@
 $folders = "temp1", "temp2", "temp3", "temp4"
 
 foreach ($folder in $folders){
- mkdir $folder # stretch goal is there somethig you can do here to say if the folder exists dont create it
+ mkdir -p $folder
  echo test > "$folder/test.txt"
 }
