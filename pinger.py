28322fddc267560c20a6cdde0bb913c0e7395e6d
Tyler Ekstrom <tylerxtreme@gmail.com>
2022-03-09 15:59:26 -0700

    updated for email

--- pinger.py
+++ pinger.py
@@ -1,11 +1,23 @@
 import datetime
 import os
+import smtplib
+import ssl
 from time import sleep
+
+port = 465 #ssl
+
 
 host = input('What is the IP? ')
 delay = input('How often do you want to check the host (seconds)? ')
 emailTo =  input('In the event that the server goes down what email would you like to have notified? ')
+password = input("Type your Password and press enter: ")
 
+context = ssl.create_default_context()
+
+  #does not send email yet but no error looking for fix
+def downEmail():
+  with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login(emailTo, password)
 
 def send_ping_request():
     currentTime = datetime.datetime.now()
@@ -15,7 +27,9 @@ def send_ping_request():
     if response == 0:
         file.write(f'UP: {status}')
     else:
-        file.write(f'DOWN:  {status}')
+        file.write(f'DOWN: {status}')
+        downEmail()
+        
     sleep(int(delay))
     send_ping_request()
 
