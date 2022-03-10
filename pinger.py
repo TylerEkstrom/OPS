40c19786087aa37b145456a855dccbadb07f2e2b
Tyler Ekstrom <tylerxtreme@gmail.com>
2022-03-10 11:59:38 -0700

    fixed email sending issue

--- pinger.py
+++ pinger.py
@@ -5,19 +5,23 @@ import ssl
 from time import sleep
 
 port = 465 #ssl
-
-
 host = input('What is the IP? ')
 delay = input('How often do you want to check the host (seconds)? ')
 emailTo =  input('In the event that the server goes down what email would you like to have notified? ')
 password = input("Type your Password and press enter: ")
-
 context = ssl.create_default_context()
 
-  #does not send email yet but no error looking for fix
-def downEmail():
+def downEmail(subject, message):
   with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
     server.login(emailTo, password)
+    server.sendmail(emailTo, emailTo, f"""
+    From: {emailTo}
+    Subject:{subject}
+
+    {message}
+    """)
+    server.close()
+    print("Email Sent")
 
 def send_ping_request():
     currentTime = datetime.datetime.now()
@@ -28,8 +32,9 @@ def send_ping_request():
         file.write(f'UP: {status}')
     else:
         file.write(f'DOWN: {status}')
-        downEmail()
-        
+        downEmail('[SERVER IS DOWN]', status)
+        exit(1)
+
     sleep(int(delay))
     send_ping_request()
 
